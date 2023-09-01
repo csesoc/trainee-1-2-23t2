f276f633f90f8c8cb3072e72628fc03f0b4aef74
Hindie Suputra <z5372377@ad.unsw.edu.au>
2023-09-01 15:38:27 +1000

    Align profile sections

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -1,63 +1,83 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 const ProfilePicture = styled('img')`
-background-color: black;
-border-radius: 50%;
-width: 200px;
-height: 200px;
-margin-top: -60px;
-`
-
-const StyledButton = styled('button')`
-background-color: #fff;
-`
+  background-color: black;
+  border-radius: 50%;
+  width: 200px;
+  height: 200px;
+  margin-top: -60px;
+`;
 
 const ProfileBackground = styled('div')`
-height: 100vh;
-`
+  height: 100vh;
+`;
 
 const Banner = styled('div')`
-background-color: gray;
-height: 20vh;
-`
+  background-color: gray;
+  height: 20vh;
+`;
 
 const ProfileName = styled('div')`
-font-size: 20px;
-`
+  font-size: 20px;
+`;
 
 const ProfileContainer = styled('div')`
-display: flex;
-`
+  display: flex;
+`;
 
 const EditButton = styled('button')`
-display: flex;
-height: 30px;
-width: 50px;
-align-items: center;
-justify-content: center;
-`
+  display: flex;
+  height: 30px;
+  width: 50px;
+  align-items: center;
+  justify-content: center;
+`;
 
 const SaveButton = styled('button')`
-display: flex;
-height: 30px;
-width: 50px;
-align-items: center;
-justify-content: center;
-`
+  display: flex;
+  height: 30px;
+  width: 50px;
+  align-items: center;
+  justify-content: center;
+`;
 
+const NameBioContainer = styled('div')`
+  margin-left: 30px;
+`;
+
+const Bio = styled('div')`
+  font-size: 15px;
+`;
+
+const DescriptionButtonContainer = styled('div')`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+const ButtonContainer = styled('div')`
+  display: flex;
+`;
 
 const ProfilePage = () => {
-return (
-  <ProfileBackground>
-    <Banner></Banner>
-    <ProfileContainer>
-      <ProfilePicture></ProfilePicture>
-      <ProfileName>Name</ProfileName>
-      <EditButton>Edit</EditButton>
-      <SaveButton>Save</SaveButton>
-    </ProfileContainer>
-  </ProfileBackground>
-)
-}
+  return (
+    <ProfileBackground>
+      <Banner></Banner>
+      <ProfileContainer>
+        <ProfilePicture></ProfilePicture>
+        <DescriptionButtonContainer>
+          <NameBioContainer>
+            <ProfileName>Name</ProfileName>
+            <Bio>Bio Something Something</Bio>
+          </NameBioContainer>
+          <ButtonContainer>
+            <EditButton>Edit</EditButton>
+            <SaveButton>Save</SaveButton>
+          </ButtonContainer>
+        </DescriptionButtonContainer>
+      </ProfileContainer>
+    </ProfileBackground>
+  );
+};
 
 export default ProfilePage;
