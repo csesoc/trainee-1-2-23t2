5daae867011a9c047843c8246f134b47d58e61d6
Hindie Suputra <z5372377@ad.unsw.edu.au>
2023-09-01 14:52:57 +1000

    Set profile background

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -1,14 +1,30 @@
+import styled from 'styled-components'
+
+const ProfilePicture = styled('img')`
+background-color: black;
+border-radius: 50%;
+width: 100px;
+height: 100px;
+`
 
 const StyledButton = styled('button')`
 background-color: #fff;
 `
 
+const ProfileBackground = styled('div')`
+height: 100vh;
+`
+
+const Banner = styled('div')`
+background-color: red;
+`
+
 const ProfilePage = () => {
 return (
-  <div>
-    Sty
-    <div className="profilePicture"></div>
-  </div>
+  <ProfileBackground>
+    <Banner></Banner>
+    <ProfilePicture></ProfilePicture>
+  </ProfileBackground>
 )
 }
 
