218368bd7c94b0bfcc830eef6ff576c4a322676f
Jasmine Tran <z5394841@ad.unsw.edu.au>
2023-09-01 13:49:30 +1000

    routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <>goodshit, deez nutz</>;
+return (
+  <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<></>} />
+  </Routes>
+</BrowserRouter>
+)
 }
 
 export default App;
