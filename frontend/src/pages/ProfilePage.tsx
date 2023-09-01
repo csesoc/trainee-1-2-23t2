c388ebe59b212a9dbb2326e68fd5368ffbfd3414
Hindie Suputra <z5372377@ad.unsw.edu.au>
2023-09-01 15:01:17 +1000

    Add profile picture and banner

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -3,8 +3,9 @@ import styled from 'styled-components'
 const ProfilePicture = styled('img')`
 background-color: black;
 border-radius: 50%;
-width: 100px;
-height: 100px;
+width: 200px;
+height: 200px;
+margin-top: -60px;
 `
 
 const StyledButton = styled('button')`
@@ -16,7 +17,8 @@ height: 100vh;
 `
 
 const Banner = styled('div')`
-background-color: red;
+background-color: gray;
+height: 20vh;
 `
 
 const ProfilePage = () => {
