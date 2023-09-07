2098aff3e7794aae97c07a499ad248bf32d3f00e
Briannica <brianim91@gmail.com>
2023-09-07 15:55:17 +1000

    small edits

--- frontend/src/styles/GlobalStyles.ts
+++ frontend/src/styles/GlobalStyles.ts
@@ -1,12 +1,17 @@
 import styled from 'styled-components';
 
-export const PrimaryBtn = styled.button`
+export const PrimaryBtn = styled.a`
     background-color: #a18276;
     color: white;
     border: none;
     border-radius: 10px;
     padding: 10px;
+    padding-top: 8px;
     box-shadow: 0px 4px 13px grey;
     display: flex;
     cursor: pointer;
+    text-decoration: none;
+    font-size: 13px;
+    align-items: center;
+    text-align: center;
 `;
