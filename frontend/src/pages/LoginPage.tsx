27ba0f4f5fa719254a2ce7dba37cfaf044e27e6d
Briannica <brianim91@gmail.com>
2023-09-16 00:27:52 +1000

    bruh

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -19,27 +19,30 @@ const Input = styled.input`
   background-color: rgb(161, 130, 118, 0.1);
   color: #a18276;
   border: none;
-  border-radius: 10px;
+  border-radius: 15px;
   padding: 10px;
   box-shadow: 0px 4px 13px grey;
   margin: 20px;
   width: 450px;
   height: 50px;
+  text-indent: 10px;
+  font-size: 20px;
 `;
 
 const FindBtn = styled.button`
   background-color: #a18276;
   color: white;
   border: none;
-  border-radius: 10px;
+  border-radius: 15px;
   padding: 10px 60px 10px 60px;
   box-shadow: 0px 4px 13px grey;
-  display: flex;
   cursor: pointer;
   text-decoration: none;
   margin: 20px;
-  width: 450px;
-  height: 50px;
+  width: 472px;
+  height: 70px;
+  text-align: center;
+  font-size: 30px;
 `;
 
 const Spacing = styled.div`
