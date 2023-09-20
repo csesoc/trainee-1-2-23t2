2afc58ea33857a0dc6e9444af82b2bf3c34348d3
Alex <z5479923@ad.unsw.edu.au>
2023-09-20 13:44:42 +1000

    change made

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import ToiletDetails from "./pages/ToiletDetailsPage";
 import ProfilePage from "./pages/ProfilePage";
+import SettingsPage from "./pages/SettingsPage";
 
 const App = () => {
 return (
@@ -16,6 +17,7 @@ return (
     <Route path="/register" element={<RegisterPage />} />
     <Route path="/toiletdeets/:id" element={<ToiletDetails />} />
     <Route path="/profile" element={<ProfilePage />} />
+    <Route path="/settings" element={<SettingsPage />} />
   </Routes>
   </BrowserRouter>
 )
